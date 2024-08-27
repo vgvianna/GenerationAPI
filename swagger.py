d1c597622029f2966296e899a641b3dec0589295
Victor Gabriel Vianna <victorviannat@gmail.com>
2024-08-27 07:49:11 -0300

    Update swagger.py

--- swagger.py
+++ swagger.py
@@ -1,11 +1,19 @@
 from flask_swagger_ui import get_swaggerui_blueprint
+import os
 
 SWAGGER_URL = "/swagger"  # URL para acessar o Swagger UI
-API_URL = "/static/swagger.json"  # Caminho para o arquivo swagger.json
+
+# Verifica se a aplicação está rodando no ambiente de produção no Render
+if os.getenv("RENDER"):
+    API_URL = "https://generationapi.onrender.com/static/swagger.json"
+else:
+    API_URL = "/static/swagger.json"  # Caminho para o arquivo swagger.json em desenvolvimento
 
 swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL, API_URL, config={
+    SWAGGER_URL,
+    API_URL,
+    config={
         "app_name": "API de Alunos",
-        "requestInterceptor": lambda request: request.url.replace("localhost:5000", "https://generationapi.onrender.com")
+        "validatorUrl": None  # Evita mensagens de erro relacionadas ao validador
     }
 )
